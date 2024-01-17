b7c346a403ccb14977da7ff5c8ee2b56d8c6db23
samuel-manners <samuel.manners@outlook.com>
2024-01-17 14:34:12 +0000

    Initial Commit - Re-Vision web app made as a PWA in React, reference to steps https://create-react-app.dev/docs/making-a-progressive-web-app/

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //add components into gif and into imports
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
