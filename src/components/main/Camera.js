28e46eefc34d87b2cfe54e35daeb21dc576cc847
samuel-manners <samuel.manners@outlook.com>
2024-01-19 20:41:20 +0000

    Restructuring to Camera, Addition of Take Picture Button and code refractoring

--- src/components/main/Camera.js
+++ src/components/main/Camera.js
@@ -1,14 +1,12 @@
 //Code Ref: https://blog.logrocket.com/using-react-webcam-capture-display-images/
-
-// import useRef, useState, useCallback, useEffect
-import { useRef, useState, useCallback, useEffect } from "react";
-// import Webcam component
-import Webcam from "react-webcam";
+import { useRef, useState, useCallback } from 'react';
+import Webcam from 'react-webcam';
+import { saveAs } from 'file-saver';
+//import { sendImage } from './functions/save.js';
 
 const CustomWebcam = () => {
   const webcamRef = useRef(null); // create a webcam reference
   const [imgSrc, setImgSrc] = useState(null); // initialize it
-
   // Captures Photo
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -19,37 +17,28 @@ const CustomWebcam = () => {
     setImgSrc(null);
   };
 
-  // Function for sending photo
-  const sendImage = async () => {
-    try {
-
-      // get the image file as a blob
-      const responses = await fetch(imgSrc);
-      const blob = await responses.blob();
-
-      // create a FormData object and append the blob
-      const formData = new FormData();
-      formData.append("image", blob, "image.jpg");
-
-      // send the request with the FormData as the body
-      const response = await fetch("http://sam141.brighton.domains/cloudvisionapi", {
-        method: "POST",
-        body: formData,
-      })
-      // handle the response
-      if (responses.ok) {
-        console.log("Image sent successfully");
-        console.log(responses);
-      } else {
-        console.error("Error sending image");
-        console.log(responses);
-      }
-
-    } catch (error) {
-      console.log("Error when sending image - error as follows:");
-      console.log(error);
-    };
+  const saveImage = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    const blob = dataURItoBlob(imageSrc);
+    saveAs(blob, "my-image.jpg");
   };
+
+  // Convert data URI to binary array
+  function dataURItoBlob(dataURI) {
+    // Split the data URI into parts
+    let parts = dataURI.split(",");
+    // Get the content type (image/jpeg)
+    let type = parts[0].split(":")[1].split(";")[0];
+    // Decode the base64 data
+    let data = atob(parts[1]);
+    // Create a binary array
+    let array = [];
+    for (var i = 0; i < data.length; i++) {
+      array.push(data.charCodeAt(i));
+    }
+    // Return a Blob object
+    return new Blob([new Uint8Array(array)], { type: type });
+  }
 
   return (
     <div className="container">
@@ -65,11 +54,11 @@ const CustomWebcam = () => {
         />
       )}
       <div className="btn-container">
-        <button>BrowsePhotos</button>
+        
         {imgSrc ? (
           <>
             <button onClick={retake}>Retake photo</button>
-            <button onClick={sendImage}>Send photo</button>
+            <button onClick={saveImage}>Save photo</button>
           </>
         ) : (
           <button onClick={capture}>Capture photo</button>
