94d0707ce8a7768ee106e39fc05460f4b59bcfd0
samuel-manners <samuel.manners@outlook.com>
2024-01-19 21:39:23 +0000

    Added searchbox to UI

--- src/components/main/Camera.js
+++ src/components/main/Camera.js
@@ -3,6 +3,7 @@ import { useRef, useState, useCallback } from 'react';
 import Webcam from 'react-webcam';
 import { saveAs } from 'file-saver';
 //import { sendImage } from './functions/save.js';
+import { dataURItoBlob } from './functions/dataURItoBlob.js';
 
 const CustomWebcam = () => {
   const webcamRef = useRef(null); // create a webcam reference
@@ -19,27 +20,10 @@ const CustomWebcam = () => {
 
   const saveImage = () => {
     const imageSrc = webcamRef.current.getScreenshot();
+    //Add from here to capture to get image saved in one hit
     const blob = dataURItoBlob(imageSrc);
     saveAs(blob, "my-image.jpg");
   };
-
-  // Convert data URI to binary array
-  function dataURItoBlob(dataURI) {
-    // Split the data URI into parts
-    let parts = dataURI.split(",");
-    // Get the content type (image/jpeg)
-    let type = parts[0].split(":")[1].split(";")[0];
-    // Decode the base64 data
-    let data = atob(parts[1]);
-    // Create a binary array
-    let array = [];
-    for (var i = 0; i < data.length; i++) {
-      array.push(data.charCodeAt(i));
-    }
-    // Return a Blob object
-    return new Blob([new Uint8Array(array)], { type: type });
-  }
-
   return (
     <div className="container">
       {imgSrc ? (
@@ -58,10 +42,12 @@ const CustomWebcam = () => {
         {imgSrc ? (
           <>
             <button onClick={retake}>Retake photo</button>
-            <button onClick={saveImage}>Save photo</button>
           </>
         ) : (
+          <>
           <button onClick={capture}>Capture photo</button>
+          <button onClick={saveImage}>Save photo</button>
+          </>
         )}
       </div>
     </div>
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,12 @@
 //App Imports
 import React from 'react';
 import Camera from '../components/main/Camera';
+import Searchbox from '../components/main/SearchBar';
 
 function App() {
   return (
     <div className="MainScreen">
+      <Searchbox/>
       <Camera/>
     </div>
   );
